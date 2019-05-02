37cd09eed9fbc51570a3ce4e0c16219f329fc140
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2019-05-02 11:33:41 +0200

    Initial

---
+++ GTRLocalLobby/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GTRLocalLobby {
+    class Program {
+        static void Main(string[] args) {
+            // port 802 for sim, 801 for semi pro
+            TcpListener server = new TcpListener(IPAddress.Any, 802);
+
+            server.Start();
+            while (true) {
+                TcpClient client = server.AcceptTcpClient();
+
+                NetworkStream ns = client.GetStream();
+
+                List<byte> connectedmsgbytelist = Encoding.ASCII.GetBytes("CONNECTED:").ToList();
+                connectedmsgbytelist.Add(0xff);
+
+                ns.Write(connectedmsgbytelist.ToArray(), 0, connectedmsgbytelist.Count);
+
+                while (client.Connected) {
+                    try {
+                        byte[] msg = new byte[1024];
+                        ns.Read(msg, 0, msg.Length);
+
+                        //string msgstr = BitConverter.ToString(msg);
+                        string msgstr = Encoding.ASCII.GetString(msg);
+                        Console.WriteLine(msgstr);
+
+                        if (msgstr.StartsWith("VERSION")) {
+                            List<byte> versionmsgbytelist = Encoding.ASCII.GetBytes("VERSION:OK").ToList();
+                            versionmsgbytelist.Add(0xff);
+
+                            ns.Write(versionmsgbytelist.ToArray(), 0, versionmsgbytelist.Count);
+
+                        }
+                        if (msgstr.StartsWith("LOGIN")) {
+                            List<byte> versionmsgbytelist = Encoding.ASCII.GetBytes("LOGIN:OK").ToList();
+                            versionmsgbytelist.Add(0xff);
+
+                            ns.Write(versionmsgbytelist.ToArray(), 0, versionmsgbytelist.Count);
+
+                        }
+                        if (msgstr.StartsWith("SETUP_LOGIN")) {
+                            List<byte> versionmsgbytelist = Encoding.ASCII.GetBytes("LOGIN:OK").ToList();
+                            versionmsgbytelist.Add(0xff);
+
+                            ns.Write(versionmsgbytelist.ToArray(), 0, versionmsgbytelist.Count);
+
+                        }
+                        if (msgstr.StartsWith("PING")) {
+                            List<byte> versionmsgbytelist = Encoding.ASCII.GetBytes("PING:").ToList();
+                            versionmsgbytelist.Add(0xff);
+
+                            ns.Write(versionmsgbytelist.ToArray(), 0, versionmsgbytelist.Count);
+
+                        }
+                        if (msgstr.StartsWith("LOGOUT")) {
+                            List<byte> versionmsgbytelist = Encoding.ASCII.GetBytes("LOGOUT:OK").ToList();
+                            versionmsgbytelist.Add(0xff);
+
+                            ns.Write(versionmsgbytelist.ToArray(), 0, versionmsgbytelist.Count);
+                            client.Close();
+                        }
+                    } catch(Exception e) {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# GTR Local Lobby
+This tool is designed to let you bypass the connection to the GTR online lobby in order to use the Direct IP feature. **The server list isn't implemented.** 
+
+## How to
+- Copy everything in _gameFolder to the game's folder.  
+  - The TXT files make the game connect to your local computer instead of the defunct global lobby server, and the exe is the local lobby server.
+- Run GTRLocalLobby.exe
+- Open GTR
+- Go to Simulation (no semi pro) => Multiplayer => LAN/Direct IP
+- Click add server at the bottom, fill in the server IP
+- When asked for credentials, use whatever it doesn't matter
+- Login should be successful
+- After a few seconds servers for the IP should appear in the list and accept connection as usual
